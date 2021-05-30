a8b93b38c9589b070cab213b1b4913e0dc7b750e
Natarajan <natarajarumuga@gmail.com>
2021-05-29 22:58:57 -0400

    API - working

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DataGov_API_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        HttpClient httpClient;
+        static string BASE_URL = "https://developer.nps.gov/api/v1";
+        static string API_KEY = "qHvKaletnN2D1eSZ2z31P3CI2Q82EDa6Jt6U743X"; //Add your API key here inside ""
+
+        //static string BASE_URL = "https://data.cdc.gov/api/views/hk9y-quqm/rows.json";
+
+        public IActionResult Index()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string NATIONAL_PARK_API_PATH = BASE_URL + "/parks?limit=20";
+            string parksData = "";
+
+            //Parks parks = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH).GetAwaiter().GetResult();
+                // HttpResponseMessage response = httpClient.GetAsync(BASE_URL)
+                //                                        .GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    parksData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!parksData.Equals(""))
+                {
+                    // JsonConvert is part of the NewtonSoft.Json Nuget package
+                    //parks = JsonConvert.DeserializeObject<Parks>(parksData);
+                }
+
+                //dbContext.Parks.Add(parks);
+               // await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataGov_API_Intro
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            //Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddMvc();
+            //services.AddMvc(options => options.EnableEndpointRouting = false);
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                //app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            //app.UseMvc(routes =>
+            //{
+             //   routes.MapRoute(
+             //      name: "default",
+             //     template: "{controller=Home}/{action=Index}/{id?}");
+            //});
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseCors();
+
+            //app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapRazorPages();
+                //endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
